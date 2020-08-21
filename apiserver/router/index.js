29ece0e29b0a34686f6390799af1f13d25d3549e
comanyu <1179128611@qq.com>
2020-08-21 15:11:01 +0800

    返回数据的处理

--- apiserver/router/index.js
+++ apiserver/router/index.js
@@ -1,9 +1,12 @@
+const comment = require('../')
 const express = require('express')
 
 const router = express.Router()
 
-router.use(express.urlencoded({extended:false}),express.json())
+router.use(express.urlencoded({
+    extended: false
+}), express.json())
 
-router.use('/user',user)
-
+router.use('/user', user)
+router.use('/comment', comment)
 module.exports = router
