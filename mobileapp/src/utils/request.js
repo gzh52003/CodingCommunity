2b711957b013c023b6116e6ff51c516540c8ec94
DDboom <a499947285@qq.com>
2020-09-10 10:09:03 +0800

    动态配置request的basUrl

--- mobileapp/src/utils/request.js
+++ mobileapp/src/utils/request.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
+const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:10000/api/' : 'http://47.100.56.31:10000/api/'
 
 const request = axios.create({
-    baseURL: 'http://47.100.56.31:10000/api/',
+    baseURL: baseURL,
     withCredentials: true
 })
 
